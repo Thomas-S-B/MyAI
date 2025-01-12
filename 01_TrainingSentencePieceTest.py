7982336ceb6047c06152530f1aabcd7c0d5a66f9
Thomas Struller-Baumann <contact@struller-baumann.de>
2025-01-12 10:56:29 +0100

    Update 01_TrainingSentencePieceTest.py

--- 01_TrainingSentencePieceTest.py
+++ 01_TrainingSentencePieceTest.py
@@ -1,6 +1,6 @@
 import sentencepiece as spm
 
-# Training auf einem Textkorpus (z. B. "train.txt")
+# Training auf einem Textkorpus 
 spm.SentencePieceTrainer.train(
     input='/home/thomas/myKI/00_Eingabe/Testtext.txt',  # Textdatei mit Rohtext
     model_prefix='/home/thomas/myKI/01_Ausgabe_Training/MeinModel',  # Präfix für das gespeicherte Modell
